d5f4581c0544872618d13a88fc52d5f25a55df35
Nicolas Quiroga Santini <103652159+Nico-Qs@users.noreply.github.com>
2022-06-04 11:26:44 -0300

    Delete ClaseTestPalindromo.py

--- UNIDAD 3 - POO/Ejercicio 9/ClaseTestPalindromo.py
+++
@@ -1,9 +0,0 @@
-from ClasePalindromo import Palindromo
-import unittest
-class TestPalindromo(unittest.TestCase):
-    __palabra: Palindromo
-    def setUp(self):
-        self.__palabra = Palindromo("MENEM")
-    def test_esPalind(self):
-        self.assertTrue(self.__palabra.esPalindromo())
-
